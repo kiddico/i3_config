2dd99c4b6e6f8b1472652d13fa1b418936f8b9b6
Nick Chirico <nickchirico07@gmail.com>
2016-11-29 19:00:30 -0500

    This might be useful, I have no idea
    Former-commit-id: 38327e4f2ecfbdd1d9e33c34d26cabf53667de31

---
+++ scripts/change_theme.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Inside of .bashrc there needs to be a line like:
+# export PCTYPE="laptop"
+# or 
+# export PCTYPE="desktop"
+# Depending on which machine we're on.
+
+# Alternatively it may be best to be a little tricky with it.
+# like maybe look through the system for a battery.
+# if we find one, then we can assume it's a laptop,
+# and a laptop modifier can be used.
+
+
+source ~/.bashrc
+
+if [ -d /sys/class/power_supply/BAT0 ];
+then
+	PCTYPE="laptop"
+else
+	PCTYPE="desktop"
+fi
+
+
+cd ~/.config/i3
+make $PCTYPE theme=$1
