fa2bf91e9a39c74aa9ad0f8f8e0e00860d4dcffd
Nick Chirico <nickchirico07@gmail.com>
2017-01-26 18:48:12 -0500

    yeah!

--- blocks/calendar.sh
+++ blocks/calendar.sh
@@ -3,3 +3,11 @@
 #   HH:MM   DD/MM/YY
 
 echo " $(date +%D)"
+
+case $BLOCK_BUTTON in
+	1) gnome-calendar --class floater;; # l click
+	2) ;; # middle clock click
+	3) ;;  # right click, mute/unmute
+	4) ;; # scroll up, increase
+	5) ;; # scroll down, decrease
+esac
