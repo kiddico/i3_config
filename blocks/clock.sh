95e5954bc93d22b6873ba7515b1ede699fbbdbd2
Nick Chirico <nickchirico07@gmail.com>
2017-01-26 19:32:20 -0500

    heyyyyyaaa

--- blocks/clock.sh
+++ blocks/clock.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Getting a terminal window to float and execute something
+#   after going through the dock is not going so well.
+case $BLOCK_BUTTON in
+1) ;; # left   click
+2) ;; # middle click
+3) ;; # right  click
+4) ;; # scroll up
+5) ;; # scroll down
+esac
+
 echo " $(date '+%R')  "
