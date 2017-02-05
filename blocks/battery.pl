eef50bf59a27125361ca678853ff687146a8fbb2
Nick Chirico <nickchirico07@gmail.com>
2017-02-05 00:41:05 -0500

    Updated the battery script to use a color when charging too, and increased the update frequency. Also increased the comment's sarcasm density. Which, as we all know, sarcasm is a vital part of a balanced git repo diet.

--- blocks/battery.pl
+++ blocks/battery.pl
@@ -10,6 +10,7 @@ my $full_text = "";
 my $short_text = "";
 my $bat_number = 0;
 my $battery_icon = "";
+my $icon ="";
 my $color = "";
 
 # read the first line of the "acpi" command output
@@ -23,7 +24,7 @@ if ($acpi !~ /: (\w+), (\d+)%/) {
 }
 
 $status = $1;
-$percent = $2;
+$percent = $2; # how does that work?...
 
 if ($percent < 5) {
 	my $temp = "!  ";
@@ -32,40 +33,42 @@ if ($percent < 5) {
 	exit(33);
 }
 
+# Choose Color, anc Battery Icon
+if ($percent < 30) {
+	$battery_icon ="  ";
+	$color="#E89254";
+
+}elsif ($percent < 60) {
+	$battery_icon ="  ";
+	$color="#E6E854";
+
+}elsif ($percent < 80) {
+	$battery_icon ="  ";
+	$color="#E6E854";
+
+}elsif ($percent < 100) {
+	$battery_icon =" ";
+	$color="#9FD84F";
+}# fi
+
+# Use the battery icon chosen above or wall plug icon
 if ($status eq 'Discharging') {
-
-	if ($percent < 30) {
-		$battery_icon ="  ";
-		$color="#9FD84F";
-	}
-	elsif ($percent < 60) {
-		$battery_icon ="  ";
-		$color="#E6E854";
-
-	}
-	elsif ($percent < 80) {
-		$battery_icon ="  ";
-		$color="#E6E854";
-
-	}
-	elsif ($percent < 99) {
-		$battery_icon ="   ";
-		$color="#E89254";
-
-	}
-
-} #End of outter if
+	$icon=$battery_icon;
+}
 elsif ($status eq 'Charging') {
-	$full_text .= '  ';
+	$icon= " ";
 }
 
-## Concatenate all the bits together
-$full_text .= "$battery_icon";
-$full_text .= "$percent";
 
-print "$full_text\n";
-print "$short_text\n";
-print $color;
+## Concatenate all the bits together
+### note: do NOT include EOL!!
+
+$full_text .= "$icon $percent";
+$short_text .= "$percent";
+
+print "$full_text\n";	#   42 /   42
+print "$short_text\n";	# 42
+print "$color\n";		# red -> green
 
 
 
