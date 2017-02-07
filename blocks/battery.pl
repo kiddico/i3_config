e891555e93305f1d1a7b1edb8a873363f893e64a
Nick Chirico <nickchirico07@gmail.com>
2017-02-07 18:12:30 -0500

    fuuuu

--- blocks/battery.pl
+++ blocks/battery.pl
@@ -35,15 +35,15 @@ if ($percent < 5) {
 
 # Choose Color, anc Battery Icon
 if ($percent < 30) {
-	$battery_icon ="  ";
+	$battery_icon ="";
 	$color="#E89254";
 
 }elsif ($percent < 60) {
-	$battery_icon ="  ";
+	$battery_icon =" ";
 	$color="#E6E854";
 
 }elsif ($percent < 80) {
-	$battery_icon ="  ";
+	$battery_icon =" ";
 	$color="#E6E854";
 
 }elsif ($percent < 100) {
