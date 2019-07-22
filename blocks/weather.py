7219b2c89e23a2fbd84e602bea869712fc086f6f
Nick Chirico <nick@srv>
2019-07-21 21:00:46 -0400

    Alrighty. Added backup source, and expanded error 'handling'

--- blocks/weather.py
+++ blocks/weather.py
@@ -5,6 +5,7 @@ import pprint
 import requests
 pp = pprint.PrettyPrinter(indent=4).pprint
 
+from sys import exit
 
 
 conditions = {
@@ -18,33 +19,55 @@ conditions = {
         'hail'       : '',
         }
 
+def c_to_f(c):
+    return int((c * 1.8) + 32)
+
+def get_with_fallback(pri, bup):
+    try:
+        temp =pri['temperature']['value'] if 'value' in pri['temperature'] else bup['temperature']['value']
+        temp = c_to_f(temp)
+    except:
+        temp = '?'
+
+    # No fallback for the actual weather.
+    # I don't care if it's raining in cleveland, but I can use it to estimate our temperature.
+    try:
+        desc = pri['textDescription'].lower()
+    except:
+        desc = None
+
+    try:
+        weather = pri['presentWeather']
+        if len(weather) > 0:
+             weather = ' '.join( item for item in weather if 'weather' in item ).lower()
+    except:
+        weather = None
+    return temp, desc, weather
+   
+
 
 def main():
-    station = 'KMFD'
 
-    url = f'https://api.weather.gov/stations/{station}/observations/latest'
-    data = json.loads(requests.get(url).content)['properties']
+    # PLEASE PAUSE FOR STATION IDENTIFICATION.
+    primary = 'KMFD'
+    backup  = 'KCLE'
 
+    url  = f'https://api.weather.gov/stations/{primary}/observations/latest'
+    primary_data = json.loads(requests.get(url).content)['properties']
 
+    url   = f'https://api.weather.gov/stations/{backup}/observations/latest'
+    backup_data  = json.loads(requests.get(url).content)['properties']
 
-    # For some reason temperature can be missing.
-    try:
-        c_temp = data['temperature']['value']
-        f_temp = int((c_temp * 1.8) + 32)
-    except:
-        f_temp = '?'
-
-
-    description = data['textDescription'].lower()
-    weather = data['presentWeather'][0]['weather'].lower()
+    temp, desc, weather = get_with_fallback(primary_data, backup_data)
 
     icons = ''
     for condition, icon in conditions.items():
-        if condition in description:
+        if condition in desc or weather:
             icons += icon
-
-    full_text  = f'{f_temp}°{" ".join(icons)}\u200B'
-    short_text = f'{f_temp}°'
+    icons = set(icons)
+    full_text  = f'{temp}°{" ".join(icons)}'
+    #full_text  = f'{temp}°{" ".join(icons)}\u2028'
+    short_text = f'{temp}°'
 
     print(full_text)
     print(short_text)
