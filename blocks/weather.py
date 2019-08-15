9aa1504494c42c747e9f81769764908ec1f29779
Nick Chirico <nick@srv>
2019-08-14 23:44:40 -0400

    Slight rework of fallback, added backbone for mouse actions, and temporary location swap.

--- blocks/weather.py
+++ blocks/weather.py
@@ -3,10 +3,10 @@
 import json
 import pprint
 import requests
+from os import environ
+from subprocess import run
+
 pp = pprint.PrettyPrinter(indent=4).pprint
-
-from sys import exit
-
 
 conditions = {
         'cloud'      : '',
@@ -19,38 +19,53 @@ conditions = {
         'hail'       : '',
         }
 
+moon = ''
+sun  = ''
+
 def c_to_f(c):
     return int((c * 1.8) + 32)
 
+# Primary and Backup Data Sources Fallback "Logic"
 def get_with_fallback(pri, bup):
+    used_fallback = False
+
+    # I <3 dict.setdefault()
+    p_temp = pri['temperature'].setdefault('value')
+    b_temp = bup['temperature'].setdefault('value')
+
     try:
-        temp =pri['temperature']['value'] if 'value' in pri['temperature'] else bup['temperature']['value']
-        temp = c_to_f(temp)
+        temp = c_to_f( p_temp if p_temp else b_temp)
     except:
+        # Handle the exception? Nah.
         temp = '?'
 
-    # No fallback for the actual weather.
-    # I don't care if it's raining in cleveland, but I can use it to estimate our temperature.
+    # Description and Present Weather used to generate icons
+    # None of this is pretty, but it works.
     try:
+        # Don't use fallback source for weather conditions.
         desc = pri['textDescription'].lower()
     except:
         desc = None
-
     try:
         weather = pri['presentWeather']
         if len(weather) > 0:
              weather = ' '.join( item for item in weather if 'weather' in item ).lower()
     except:
         weather = None
-    return temp, desc, weather
+
+    return temp, desc, weather, used_fallback
    
 
 
 def main():
 
     # PLEASE PAUSE FOR STATION IDENTIFICATION.
-    primary = 'KMFD'
-    backup  = 'KCLE'
+    # Mansfield / Cleveland
+    #primary = 'KMFD'
+    #backup  = 'KCLE'
+    # Columbus: John Glenn/Zoo
+    primary = 'KCMH'
+    backup  = 'KOSU'
 
     url  = f'https://api.weather.gov/stations/{primary}/observations/latest'
     primary_data = json.loads(requests.get(url).content)['properties']
@@ -58,27 +73,36 @@ def main():
     url   = f'https://api.weather.gov/stations/{backup}/observations/latest'
     backup_data  = json.loads(requests.get(url).content)['properties']
 
-    temp, desc, weather = get_with_fallback(primary_data, backup_data)
+    temp, desc, weather, used_fallback = get_with_fallback(primary_data, backup_data)
 
     icons = ''
     for condition, icon in conditions.items():
         if condition in desc or weather:
             icons += icon
+
     icons = set(icons)
     full_text  = f'{temp}°{" ".join(icons)}'
-    #full_text  = f'{temp}°{" ".join(icons)}\u2028'
     short_text = f'{temp}°'
 
-    print(full_text)
-    print(short_text)
+    # Use a scawwy color if we had to use the fallback source
+    color = '#EABD6F' if used_fallback else '#6CA8AC'
+    print(f'{full_text}\n{short_text}\n{color}')
 
 
+    # Handle mouse actions
+    try:
+        env_str = 'BLOCK_BUTTON'
+        mouse_action = environ[env_str] if env_str in environ else None
+        handle_action(mouse_action)
+    except Exception as e:
+        print(e)
 
 
-
-
-
-
+def handle_action(mouse_action):
+    if mouse_action:
+        # Left Click
+        if mouse_action == '1': 
+            run(['gnome-weather --class floater'], shell=True )
 
 
 
