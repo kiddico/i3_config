eeb0ac08f73543b4f5acb0d6627b43d94c39a206
kiddico <nickchirico07@gmail.com>
2023-12-24 01:47:41 -0500

    Smaller?

--- scripts/screen_reconing.sh
+++ scripts/screen_reconing.sh
@@ -4,14 +4,14 @@ external_display="$(xrandr --listmonitors | egrep -o '( DP-[1-4])')"
 if [ "$external_display" == " DP-4" ]
 then
 	echo "1"
-	xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 3840x2160 --pos 0x0 --rotate normal --primary
+	xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 3840x2160 --pos 0x0 --rotate inverted --primary
 	# 31.5" @ 3840x2160 = 139.87 PPI
 	xrandr --dpi 120
 
 elif [ "$external_display" == " DP-3" ]
 then
 	echo "2"
-	xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --primary --output DP-4 --off
+	xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --mode 3840x2160 --pos 0x0 --rotate inverted --primary --output DP-4 --off
 	# 31.5" @ 3840x2160 = 139.87 PPI
 	xrandr --dpi 120
 
@@ -20,8 +20,6 @@ else
 	xrandr --output eDP-1 --primary --mode 2256x1504 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
 	# I think the DPI for the framework is 200?
 	# 13.5" diagonal @ 2256x1504 = 200.84 PPI
-	xrandr --dpi 200
+	xrandr --dpi 160
 
 fi
-
-sleep 1
